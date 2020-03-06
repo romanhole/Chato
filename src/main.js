88c76941127015892660d1479fb89017e019f194
RAFAEL ROMANHOLE BORROZINO <u19196@academico.cotuca.unicamp.br>
2020-03-06 15:17:27 -0300

    Começando o teste com o tutorial basic chat vue.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,33 @@ import App from './App.vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
+import PubNubVue from 'pubnub-vue'
 
+Vue.config.productionTip = false;
+    
+const publish_Key = 'YOUR_PUBLISH_KEY_HERE';
+const subscribe_Key = 'YOUR_SUBSCRIBE_KEY_HERE';
+
+// Make a unique uuid for each client
+const myUuid = fourCharID();
+const me = {
+    uuid: myUuid,
+};
+
+try{
+    if(!publish_Key || !subscribe_Key){
+        throw 'PubNub Keys are missing.';
+    }
+}
+catch(err){
+    console.log(err);
+}
+
+Vue.use(PubNubVue, {
+  subscribeKey: subscribe_Key,
+  publishKey: publish_Key,
+  ssl: true
+}, store);
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
@@ -14,5 +40,9 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  store,
+  components: { App },
+  //render: h => h(App),
+  template: '&lt;App/&gt;',
+  created
 })
