346091e624b77ba999828c6d48620957655684eb
Marcelo Sicoli <49480702+Marcelo-Sicoli@users.noreply.github.com>
2020-03-12 11:55:22 -0300

    home iniciada

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
-    
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#sobre">sobre</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h2>Chato<h2>
+    <div id="animacao">
+      <p>
+        Tá achando o mundo real muito chato?<br>
+        Venha se divertir com seus amigos virtualmente
+      </p>
+    </div>
+    <button>clique aqui para cadastrar</button>
+    <!-- <img> -->
+    <section class="contact-section bg-black" id="sobre">
+      <div id="sobre">
+      
+      </div>
+    </section>
+    <div id="contato">
+      
+    </div>
+
   </div>
 </template>
 
