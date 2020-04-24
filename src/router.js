b2d0970333becd6e2f59739810c8bc9cc6e9c597
Rafael Romanhole Borrozino <49032796+romanhole@users.noreply.github.com>
2020-04-23 22:47:53 -0300

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import Home from './components/home/Home.vue';
 import Cadastro from './components/cadastro/Cadastro.vue';
+import Chat from './components/chat/Chat.vue';
+import Login from './components/login/Login.vue';
 
 export const routes = [
 
     { path: '', component: Home},
-    { path: '/cadastro', component: Cadastro}
+    { path: '/cadastro', component: Cadastro},
+    { path: '/login', component: Login},
+    { path: '/chat', component: Chat}
 
 ]
