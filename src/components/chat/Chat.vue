e2e2658e59e5261f54876d1e7f043e4697dc4222
Rafael Romanhole Borrozino <49032796+romanhole@users.noreply.github.com>
2020-04-23 22:27:09 -0300

    Add files via upload

---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,52 @@
+<template>
+<div id="container">
+    <div class="conteudo">
+        <div id="primeiraColuna">
+            <p>Teste</p>
+        </div>
+        <div id="segundaColuna">
+            <input type="text">
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .conteudo{
+    border-radius: 15px;
+    background-color: white;
+    width: 1400px;
+    height: 75%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    }
+    .conteudo::before{
+    content: "";
+    position: absolute;
+    background-color: #8B0000;
+    width: 20%;
+    height: 100%;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+    }
+    #container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+    }
+    input{
+    }
+    #segundaColuna{
+        align-items: center;
+    }
+</style>
