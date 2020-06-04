01835864f5917ae5b4ffdfbd97510f9236343329
Rafael Romanhole Borrozino <49032796+romanhole@users.noreply.github.com>
2020-06-04 20:07:49 -0300

    Add files via upload

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,63 +1,179 @@
 <template>
-<div id="caixa">
-    <div class="cont">
-        <div id="Coluna1">
-            <p>Teste</p>
-        </div>
-        <div id="Coluna2">
-            <input type="text">
-        </div>
-    </div>
+<div id="teste">
+  <div class="todo">
+      <div class="item article-01">
+          <div class="left">
+              <div class="left-01">
+              </div>
+              <div class="left-02">
+                  <input type="text" id="procurar">
+              </div>
+              <div class="left-03">
+              </div>
+          </div>
+      </div>
+      <div class="item article-02">
+          <div class="right">
+              <div class="right-01">
+                  <input type="text" id="msg">
+              </div>
+              <div class="right-02">
+                <!--<div v-for="message in messages" :key="message.Id" v-bind="message" :class="{['recebida']: message.recieved, ['enviada']: message.sended}">
+                    <h1 v-if="message.grupo">{{ message.Nome }}</h1>
+                    <p>{{ message.Text }}</p>
+                </div>-->
+                <div class="recebida">
+                    <h1>João</h1>
+                    <p>Ola, amigos tudo bem?</p>
+                </div>                          
+                <div class="mensagem enviada">
+                    <h1>Eu</h1>
+                    <p>orem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in condimentum nulla. Mauris convallis nisl sed pharetra suscipit. Vivamus euismod pulvinar leo quis gravida. Curabitur in magna sem.</p>
+                </div>                                              
+              </div>
+              <div class="right-03">
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            messages:[]
+        }
+    }
 }
+/*window.onload = function(){
+    var conectado = false;
+    if(!conectado)
+        HttpResponse.RedirectToRoute('/login');
+    window.setInterval(1000,atualizar());
+}
+function atualizar(){
+    //atualizar o chat a cada 1 seg pra ver se tem mensagem nova.
+}*/
 </script>
 
-<style>
-    .cont{
-    border-radius: 15px;
-    background-color: white;
-    width: 1400px;
-    height: 75%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
-    }
-    .cont::before{
-    content: "";
-    position: absolute;
-    background-color: #8B0000;
-    width: 30%;
-    height: 100%;
-    border-top-left-radius: 15px;
-    border-bottom-left-radius: 15px;
-    }
-    #caixa{
+<style scoped>
+#teste{
+    width: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+}
+.todo{
+    height: 800px;
+    width: 1400px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+.article-01{
+    width: 25%;
+    background-color: #8B0000;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+}
+.article-02{
+    width: 75%;
     background-color: #f0f0f0;
-    }
-    input{
-        float: bottom;
-    }
-    #Coluna1{
-        flex: 2 0 auto;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        order: 1;
-    }
-    #Coluna2{
-        text-align: center;
-        flex: 1.5 0 auto;
-        z-index: 10;
-        order: 2;
-    }
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+}
+
+.left{
+    display: flex;
+    flex-direction: column;
+}
+.left-01{
+    height: 60px;
+    background-color: rgb(100, 1, 1);
+    border-top-left-radius: 15px;
+}
+.left-02{
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid rgb(100, 1, 1);
+}
+.left-03{
+    height: 690px;
+}
+#procurar{
+    border-radius: 7px;
+    border-style: none;
+    outline: none;
+    width: 280px;
+    height: 20px;
+}
+.right{
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+}
+#msg{
+    width: 750px;
+    height: 35px;
+    border-radius: 7px;
+    border-style: none;
+    outline: none;
+    font-size: 15px;
+}
+.right-01{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 60px;
+    background-color: lightgrey;
+    justify-content: center;
+    align-items: center;
+    border-bottom-right-radius: 15px;
+}
+.right-02{
+    height: 680px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: 15px;
+}
+.right-02>div{
+    display: flex;
+    flex-direction: column;
+    max-width: 40%;
+    background-color: white;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+}
+.right-02>div h1{
+    font-size: 17px;
+    color: blue;
+    margin-bottom: 3px;
+}
+.right-02>div p{
+    text-align: justify;
+}
+.recebida{
+    align-self: flex-start;
+}
+.enviada{
+    align-self: flex-end;
+}
+.right-03{
+    height: 60px;
+    background-color: lightgray;
+    border-top-right-radius: 15px;
+}
 </style>
