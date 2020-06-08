da7bf6f110b255af0484d356b4255699fb8c90ea
Rafael Romanhole Borrozino <49032796+romanhole@users.noreply.github.com>
2020-06-08 08:13:25 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
-import VueResource from "vue-resource";
-import VueRouter from "vue-router";
-import { routes } from "./router";
-import "./assets/style.css";
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import { routes } from './router'
+import './assets/style.css'
 import Axios from "axios";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.prototype.$http = Axios;
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: routes
+  routes : routes
 });
 
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   render: h => h(App)
-});
+})
