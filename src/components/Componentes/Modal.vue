94cd2076895aca6d01f7fd6711ed5c08b5de11d5
Rafael Romanhole Borrozino <49032796+romanhole@users.noreply.github.com>
2020-06-13 00:37:21 -0300

    Add files via upload

---
+++ src/components/Componentes/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <div class="container" :class="{'open': open}">
+          <div>
+              <div>
+                  <p>Ola</p>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            open: false
+        }
+    },
+    methods:{
+
+    },
+    name: 'Modal'
+}
+</script>
+
+<style>
+    .container{
+        position: fixed;
+        display: none;
+    }
+    .container>div{
+        position: fixed;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0
+    }
+    .container>div>div{
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+    }
+</style>
